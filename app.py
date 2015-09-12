aab75ca7b3062b37f8d7d3426c3c771a6075d84d
Pranav Sathy <sathyp@rpi.edu>
2015-09-12 15:59:37 -0400

    Politik backend working

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.cors import CORS
 from newspaper import Article
-import predictionio
 import json
 import math
+
+political_keywords = ['iran', 'obama', 'campaign', 'resolution', 'trump', 'republicans', 'democrats', 'bernie', 'election', 'voting', 'polls', 'hillary', 'bush', 'war', 'economy', 'abortion', 'gun', 'rights', 'conservative', 'liberal', 'liberty', 'privacy', 'cruz', 'santorum', 'biden', 'rubio', 'perry', 'israel']
 
 app = Flask(__name__)
 CORS(app)
@@ -15,9 +16,11 @@ class User(db.Model):
 	__tablename__ = "user"
 	id = db.Column(db.String(64), primary_key=True)
 	viewings = db.relationship('Viewing', backref='user', lazy='dynamic')
+	leaning = db.Column(db.Float)	
 
 	def __init__(self, id):
 		self.id = id
+		self.leaning = 0
 
 	def __repr__(self):
 		return '<User %r>' % self.id
@@ -27,34 +30,27 @@ class Viewing(db.Model):
 	id = db.Column(db.Integer, primary_key=True)	
 	user_id = db.Column(db.String(64), db.ForeignKey('user.id'))
 	url = db.Column(db.String(1024))
-	user_score = db.Column(db.Integer)
-	ml_score = db.Column(db.Float)
 	
 	def get_weighted_score(self):
 		return self.ml_score * self.user_score
 
-@app.route('/get_article_leaning', methods=['POST'])
-def get_article_leaning():
-	data = json.loads(request.data)
+class PoliticalSite(db.Model):
+	__tablename__ = "political_site"
 
-	results = get_ml_data(data['url'])	
-
-	return jsonify(**results)
+	id = db.Column(db.Integer, primary_key=True)
+	site_url = db.Column(db.String(1024))
+	leaning = db.Column(db.Float)
 
 def get_ml_data(url):
 	article = Article(url)
 	article.download()
 	article.parse()
 	article.nlp()
-
+	
 	article = (article.summary).encode('utf-8').strip()
-	
-	ec = predictionio.EngineClient()
-	results = ec.send_query({'text': article})
+	sentences = article.split('\n')	
+	result = 0	
 
-	if math.isnan(results['confidence']):
-		results['confidence'] = 0
-	
 	return results
 
 @app.route('/store_view', methods=['POST'])
@@ -69,36 +65,35 @@ def store_view():
 	views = db.session.query(Viewing) \
 			.filter(Viewing.url == data['url']) \
 			.filter(Viewing.user_id == data['id'])
-	results = get_ml_data(data['url'])
+	
 	if not views.count():
 		new_view = Viewing()
 		new_view.user_id = data['id']
 		new_view.url = data['url']
-		new_view.user_score = (data['score'] * 2) - 1
-		new_view.ml_score =  resuluts['confidence']
 
 		db.session.add(new_view)
-		db.session.add(commit)
+		db.session.commit()
 	
+	user = db.session.query(User).filter(User.id == data['id']).first()
+	
+	user_views = db.session.query(Viewing).filter(Viewing.user_id == user.id).all()
+	user_score = 0
+	pol_len = 0
+	urls = [view.url for view in user_views]
+	
+	pols = PoliticalSite.query.all()
+	for pol in pols:
+		for url in urls:
+			if pol.site_url in url:
+				user_score += pol.leaning
+				pol_len += 1
+	if pol_len:
+		user_score = user_score / pol_len
+		user.leaning = user_score
+	
+	db.session.commit()
 
-		return jsonify(success=True)
-	else:
-		view = views.first()
-		view.user_score = (data['score'] * 2) - 1
-		return jsonify(success=True)
-
-@app.route('/calc_leaning', methods=['POST'])
-def calc_leaning():
-	data = json.loads(request.data)
-	result = 0
-	
-	viewings = Viewing.query.filter_by(user_id=data['id']).all()
-	for viewing in viewings:
-		result += viewing.get_weighted_score()
-	if len(viewings):
-		result = result / len(viewings)
-	
-	return jsonify(value=result)
+	return jsonify(uscore=user_score)
 
 if __name__ == '__main__':
 	app.debug = True
