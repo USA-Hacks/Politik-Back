30819fdf490e891757407e871dee20fcafa74813
Pranav Sathyanarayanan <sathyp@rpi.edu>
2015-09-12 02:45:50 -0400

    Untested - document a user view

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/user'
@@ -10,6 +11,9 @@ class User(db.Model):
 	id = db.Column(db.String(32), primary_key=True)
 	viewings = db.relationship('Viewing', backref='user', lazy='dynamic')
 
+	def __init__(self, id):
+		self.id = id
+
 	def __repr__(self):
 		return '<User %r>' % self.id
 
@@ -17,10 +21,37 @@ class Viewing(db.Model):
 	__tablename__ = "viewing"
 	user_id = db.Column(db.String(32), db.ForeignKey('user.id'))
 	url = db.Column(db.String(1024))
-	score = db.Column(db.Integer)
+	user_score = db.Column(db.Integer)
+	ml_score = db.Column(db.Float)
 
-@app.route('/leaning')
-def leaning():
+@app.route('/store_view', methods=['POST'])
+def store_view():
+	data = json.loads(request.data)
+
+	if not db.session.query(User).filter(User.id == data['id']).count():
+		new_user = User(data['id'])
+		db.session.add(new_user)
+		db.session.commit()
+
+	view_count = db.session.query(Viewing) \
+			.filter(Viewing.url == data['url']) \
+			.filter(Viewing.user_id == data['id']) \
+			.count()
+
+	if not view_count:
+		new_view = Viewing()
+		new_view.user_id = data['id']
+		new_view.url = data['url']
+		new_view.user_score = data['score']
+		new_view.ml_score = 1 # Needs to be PredictionIO call
+
+		db.session.add(new_view)
+		db.session.add(commit)
+
+	return jsonify(success=True)
+
+@app.route('/calc_leaning')
+def calc_leaning():
 	ret = {
 		'leaning': 'Democrat',
 		'score': 1.3
