544972a7e49a5cf4a1cecc48db552832d1804e7a
Pranav Sathyanarayanan <sathyp@rpi.edu>
2015-09-12 22:50:40 -0400

    Revert

--- app.py
+++ app.py
@@ -6,12 +6,39 @@ import json
 
 from ml.naive_bayes import NaiveBayes
 
-from models import User, Viewing, PoliticalSite
-
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://politik:politik@localhost/politik_db'
 db = SQLAlchemy(app)
+
+class User(db.Model):
+	__tablename__ = "user"
+	id = db.Column(db.String(64), primary_key=True)
+	viewings = db.relationship('Viewing', backref='user', lazy='dynamic')
+	leaning = db.Column(db.Float)	
+
+	def __init__(self, id):
+		self.id = id
+		self.leaning = 0
+
+	def __repr__(self):
+		return '<User %r>' % self.id
+
+class Viewing(db.Model):
+	__tablename__ = "viewing"
+	id = db.Column(db.Integer, primary_key=True)	
+	user_id = db.Column(db.String(64), db.ForeignKey('user.id'))
+	url = db.Column(db.String(1024))
+		
+	def get_weighted_score(self):
+		return self.ml_score * self.user_score
+
+class PoliticalSite(db.Model):
+	__tablename__ = "political_site"
+
+	id = db.Column(db.Integer, primary_key=True)
+	site_url = db.Column(db.String(1024))
+	leaning = db.Column(db.Float)
 
 
 def get_nlp_data(url):
